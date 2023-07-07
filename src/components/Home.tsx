ba1ba03462afbd9cc3e63d9103fd4049284d09c1
Jianshao <787903+jianshaow@users.noreply.github.com>
2023-07-07 11:22:41 +0000

    store credential to local storage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ChangeEvent, FormEvent } from 'react';
+import React, { Component, ChangeEvent, FormEvent, MouseEvent } from 'react';
 import './Home.css'
 import * as cbor from 'cbor-x'
 
@@ -7,24 +7,42 @@ interface HomeState {
   loggedIn: boolean;
   userId: string;
   username: string;
+  allowCredentials: PublicKeyCredentialDescriptor[];
+  storedCredentials: Credential[];
+  displayName: string;
   rpId: string;
-  credential: PublicKeyCredential | null;
   registerEnabled: boolean;
-  loginEnabled: boolean;
+}
+
+interface Credential {
+  id: string;
+  userId: string;
+  username: string;
+  displayName: string;
+  publicKey: string;
+  publicKeyAlgorithm: number;
 }
 
 class Home extends Component<{}, HomeState> {
   constructor(props: {}) {
     super(props);
+
+    const credentialsJson = localStorage.getItem('credentials');
+    var credentials = [];
+    if (credentialsJson) {
+      credentials = JSON.parse(credentialsJson);
+    }
+
     this.state = {
       log: '',
       loggedIn: false,
       userId: '14562550-a677-4832-9add-77527ae332db',
-      username: 'admin',
+      username: 'John.Smith@TechGenius.com',
+      allowCredentials: [],
+      storedCredentials: credentials,
       rpId: window.location.host,
-      credential: null,
+      displayName: 'John Smith',
       registerEnabled: false,
-      loginEnabled: false,
     };
   }
 
@@ -43,6 +61,10 @@ class Home extends Component<{}, HomeState> {
     return new TextDecoder('utf-8').decode(value);
   }
 
+  stringToBuffer(value: string): ArrayBuffer {
+    return new TextEncoder().encode(value);
+  }
+
   bufferToBase64URLString(buffer: ArrayBuffer): string {
     const bytes = new Uint8Array(buffer);
     let str = '';
@@ -54,6 +76,24 @@ class Home extends Component<{}, HomeState> {
     const base64String = btoa(str);
 
     return base64String.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+  }
+
+  base64URLStringToBuffer(base64URLString: string): ArrayBuffer {
+    const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/');
+
+    const padLength = (4 - (base64.length % 4)) % 4;
+    const padded = base64.padEnd(base64.length + padLength, '=');
+
+    const binary = atob(padded);
+
+    const buffer = new ArrayBuffer(binary.length);
+    const bytes = new Uint8Array(buffer);
+
+    for (let i = 0; i < binary.length; i++) {
+      bytes[i] = binary.charCodeAt(i);
+    }
+
+    return buffer;
   }
 
   concat(arrays: Uint8Array[]): Uint8Array {
@@ -70,6 +110,33 @@ class Home extends Component<{}, HomeState> {
     return toReturn;
   }
 
+  generateUUID(): string {
+    const array = new Uint8Array(16);
+    crypto.getRandomValues(array);
+    array[6] = (array[6] & 0x0f) | 0x40;
+    array[8] = (array[8] & 0x3f) | 0x80;
+    const hex = Array.from(array, byte => byte.toString(16).padStart(2, '0'));
+    const uuid = `${hex.slice(0, 4).join('')}-${hex.slice(4, 6).join('')}-${hex.slice(6, 8).join('')}-${hex.slice(8, 10).join('')}-${hex.slice(10).join('')}`;
+    return uuid;
+  }
+
+  isSelected = (credentialId: string) => {
+    const { allowCredentials } = this.state;
+    return allowCredentials.some((selectedCredential) => new TextDecoder().decode(selectedCredential.id) === credentialId);
+  };
+
+  deleteCredential = (e: MouseEvent<HTMLButtonElement>) => {
+    const { storedCredentials } = this.state;
+    const newCredentials = storedCredentials.filter((credential) => credential.id !== e.target.id);
+    this.setState({ storedCredentials: newCredentials });
+    localStorage.setItem('credentials', JSON.stringify(newCredentials));
+  }
+
+  regenUserId = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    this.setState({ userId: this.generateUUID() });
+  };
+
   handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
     this.setState({ username: e.target.value });
   };
@@ -78,14 +145,31 @@ class Home extends Component<{}, HomeState> {
     this.setState({ rpId: e.target.value });
   };
 
+  handleAllowCredentialsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { allowCredentials } = this.state;
+    if (e.target.checked) {
+      const allowCredential: PublicKeyCredentialDescriptor = {
+        type: 'public-key',
+        id: this.stringToBuffer(e.target.value),
+        transports: ['internal']
+      }
+      allowCredentials.push(allowCredential);
+      this.setState({ allowCredentials: allowCredentials })
+    } else {
+      const newCredentials = allowCredentials.filter((selectedCredential) => this.bufferToUTF8String(selectedCredential.id as ArrayBuffer) !== e.target.value);
+      this.setState({ allowCredentials: newCredentials });
+    }
+  };
+
   handleRegister = async (e: FormEvent) => {
     e.preventDefault();
-    const { username, userId, rpId } = this.state;
+    const { username, userId, rpId, storedCredentials } = this.state;
 
     try {
       this.appendToLog('Start register...')
       this.appendToLog('username=' + username)
       this.appendToLog('userId=' + userId);
+
       const challenge = new Uint8Array(32);
       crypto.getRandomValues(challenge);
       const createCredentialOptions: CredentialCreationOptions = {
@@ -120,6 +204,11 @@ class Home extends Component<{}, HomeState> {
       this.appendToLog('attestation.publicKeyAlgorithm=' + attestationResponse.getPublicKeyAlgorithm());
       this.appendToLog('attestation.transports=' + attestationResponse.getTransports());
 
+      const pubclicKeyDer = attestationResponse.getPublicKey();
+      if (!pubclicKeyDer) {
+        throw new Error('no public key');
+      }
+
       const { clientDataJSON, attestationObject } = attestationResponse;
 
       // verify challenge
@@ -134,7 +223,18 @@ class Home extends Component<{}, HomeState> {
       this.appendToLog('attestationObject.fmt=' + attestationObj.fmt);
 
       // save credential in state
-      this.setState({ registerEnabled: false, loginEnabled: true, credential: credential });
+      this.setState({ registerEnabled: false });
+
+      storedCredentials.push({
+        id: credential.id,
+        userId: userId,
+        username: username,
+        displayName: '',
+        publicKey: this.bufferToBase64URLString(pubclicKeyDer),
+        publicKeyAlgorithm: attestationResponse.getPublicKeyAlgorithm()
+      });
+      localStorage.setItem('credentials', JSON.stringify(storedCredentials));
+
       this.appendToLog('Register success');
     } catch (error) {
       console.error(error);
@@ -142,29 +242,24 @@ class Home extends Component<{}, HomeState> {
       this.appendToLog('Error=' + error);
     }
 
-    this.setState({ username: 'admin' });
+    this.setState({ username: 'John.Smith@TechGenius.com' });
   };
 
   handleLogin = async (e: FormEvent) => {
     e.preventDefault();
-    const { username, credential } = this.state;
+    const { username, storedCredentials, allowCredentials } = this.state;
 
     try {
       this.appendToLog('Start login...')
       this.appendToLog('username=' + username)
-      this.appendToLog('credential.id=' + credential?.id)
+      this.appendToLog('storedCredentials.length=' + storedCredentials.length)
+
       const challenge = new Uint8Array(32);
       crypto.getRandomValues(challenge);
       const getCredentialOptions: CredentialRequestOptions = {
         publicKey: {
           challenge: challenge,
-          // allowCredentials: [
-          //   {
-          //     type: 'public-key',
-          //     id: credential?.rawId as BufferSource,
-          //     transports: ['internal', 'ble', 'nfc', 'usb', 'hybrid']
-          //   },
-          // ],
+          allowCredentials: allowCredentials,
           userVerification: 'preferred',
         },
       };
@@ -174,8 +269,9 @@ class Home extends Component<{}, HomeState> {
       this.appendToLog('assertion.type=' + assertion.type);
 
       const { authenticatorData, signature, clientDataJSON, userHandle } = assertion.response as AuthenticatorAssertionResponse;
-      if (userHandle) {
-        this.appendToLog('userHandle=' + this.bufferToUTF8String(userHandle));
+
+      if (!userHandle) {
+        throw new Error('no user id')
       }
 
       // verify challenge
@@ -189,13 +285,21 @@ class Home extends Component<{}, HomeState> {
       const signatureBase = this.concat([new Uint8Array(authenticatorData), new Uint8Array(hashedClientData)]);
 
       // retrieve public key from attestation that returned from registering before
-      if (!credential) {
-        throw new Error('No credential, register first');
+      if (!storedCredentials.length) {
+        throw new Error('No credential stored, register first');
       }
 
-      const attestationResponse = credential.response as AuthenticatorAttestationResponse;
-      const publicKeyDer = attestationResponse.getPublicKey();
-      const algorithm = attestationResponse.getPublicKeyAlgorithm();
+      const userId = this.bufferToUTF8String(userHandle);
+      this.appendToLog('userId=' + userId);
+      const filteredCredentials = storedCredentials.filter((credential) => credential.userId === userId);
+      if (!filteredCredentials.length) {
+        throw new Error('no this user stored')
+      }
+      const credential = filteredCredentials[0];
+
+      const publicKeyDer = this.base64URLStringToBuffer(credential.publicKey);
+      const algorithm = credential.publicKeyAlgorithm;
+      this.appendToLog('publicKeyAlgorithm=' + algorithm);
 
       const getImportAlgorithm = (algorithm: number): RsaHashedImportParams | EcKeyImportParams | AlgorithmIdentifier => {
         if (algorithm === -7) { // for iOS
@@ -250,12 +354,11 @@ class Home extends Component<{}, HomeState> {
       this.appendToLog('Error=' + error);
     }
 
-    this.setState({ username: 'admin' });
+    this.setState({ username: 'John.Smith@TechGenius.com' });
   };
 
   render() {
-    const { loggedIn, username, rpId, registerEnabled, loginEnabled, log } = this.state;
-
+    const { loggedIn, userId, username, storedCredentials, displayName, rpId, registerEnabled, log } = this.state;
     return (
       <div className="container">
         <div className="center">
@@ -268,25 +371,64 @@ class Home extends Component<{}, HomeState> {
             <div>
               <h1>Login</h1>
               <form onSubmit={this.handleLogin}>
-                <label> Username: </label>
-                <input type="text" value={username} onChange={this.handleUsernameChange} />
-                <br />
+                <label>storedCredentials:</label>
+                <div className='frame'>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>allowCredentials</th>
+                        <th>credentials</th>
+                        <th>action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {storedCredentials.map((credential) => (
+                        <tr key={credential.id}>
+                          <td>
+                            <input type="checkbox" value={credential.id} checked={this.isSelected(credential.id)} onChange={this.handleAllowCredentialsChange} />
+                          </td>
+                          <td>
+                            {credential.username}
+                          </td>
+                          <td>
+                            <button id={credential.id} onClick={this.deleteCredential}>delete</button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div>
+                  <label> Username: </label>
+                  <input type="text" value={username} onChange={this.handleUsernameChange} />
+                </div>
                 {registerEnabled ? (
                   <button type="submit">Register Passkey</button>
                 ) : (
-                  <button type="submit" disabled={!loginEnabled}>
-                    Passkey Login
-                  </button>
+                  <button type="submit" disabled={!storedCredentials.length}>Passkey Login</button>
                 )}
               </form>
               {registerEnabled ? null : (
                 <div>
                   <br />
-                  <label>No Passkey？</label>
+                  <label>No Passkey?</label>
                   <form onSubmit={this.handleRegister}>
-                    <label>RPId: </label>
-                    <input type="text" value={rpId} onChange={this.handleRpId} />
-                    <button type="submit" >Register Passkey</button>
+                    <div>
+                      <label> UserId: </label>
+                      <input type="text" value={userId} readOnly style={{ width: '260px' }} />
+                      <button onClick={this.regenUserId}>Regen</button>
+                    </div>
+                    <div>
+                      <label> DisplayName: </label>
+                      <input type="text" value={displayName} readOnly style={{ width: '260px' }} />
+                    </div>
+                    <div>
+                      <label>RPId: </label>
+                      <input type="text" value={rpId} onChange={this.handleRpId} />
+                    </div>
+                    <div>
+                      <button type="submit">Register Passkey</button>
+                    </div>
                   </form>
                 </div>
               )}
